22d17539846bbc6fdf123588346c70b8bbde7f18
Jason Huang <jason97521@gmail.com>
2023-04-18 14:55:54 -0400

    fetching data from mysql

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,32 @@
 import express from "express"
-import mysql from "mysql"
+import mysql from "mysql2"
+import * as dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
+app.use(express.json());
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    database:"test"
-})
+    host: "localhost",
+    user: "root",
+    password: process.env.DB_PW,
+    database: "test",
+  });
 
 app.get("/", (req,res)=>{
     res.json("hello this is the backend!")
 })
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books";
+    db.query(q, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.json(err);
+      }
+      return res.json(data);
+    });
+  });
 
 app.listen(8800, ()=>{
     console.log("Connected to backend!")
