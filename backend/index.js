262f22fa839bb890ad749c96148ae0a1a358928f
Jason Huang <jason97521@gmail.com>
2023-04-19 00:26:05 -0400

    fetch data from mysql

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from "express"
 import mysql from "mysql2"
 import * as dotenv from 'dotenv'
+import cors from "cors"
 
 dotenv.config()
 const app = express()
 app.use(express.json());
+app.use(cors())
 
 const db = mysql.createConnection({
   host: "localhost",
--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Add = () => {
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  })
+
   return (
-    <div>Add</div>
+    <div className='form'>
+      <h1>Add New Book</h1>
+      <input type="text" placeholder='title' />
+      <input type="text" placeholder='desc' />
+      <input type="number" placeholder='price' />
+      <input type="text" placeholder='cover' />
+    </div>
   )
 }
 
