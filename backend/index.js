05a1223bc365d50e64342d6ebc32fdb187c08604
Jason Huang <jason97521@gmail.com>
2023-04-19 02:39:17 -0400

    delete operation from mysql db and client side

--- backend/index.js
+++ backend/index.js
@@ -30,13 +30,23 @@ app.get("/books", (req, res) => {
   });
 });
 
-app.post("/books", (req,res)=>{
+app.post("/books", (req, res) => {
   const q = "INSERT INTO books (`title`,`desc`,`price`,`cover`) VALUES (?)"
-  const values = [req.body.title,req.body.desc,req.body.price,req.body.cover]
+  const values = [req.body.title, req.body.desc, req.body.price, req.body.cover]
 
-  db.query(q,[values],(err,data)=>{
+  db.query(q, [values], (err, data) => {
     if (err) return res.json(err)
     return res.json("Book has been created successfully")
+  })
+})
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id
+  const q = "DELETE FROM books WHERE id = ?"
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.json(err)
+    return res.json("Book has been deleted successfully")
   })
 })
 
