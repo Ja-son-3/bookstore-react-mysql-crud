bb327700d6d12d3e586baa0986c01716db67d66f
Jason Huang <jason97521@gmail.com>
2023-04-18 23:56:51 -0400

    create/post backend

--- backend/index.js
+++ backend/index.js
@@ -7,27 +7,37 @@ const app = express()
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: process.env.DB_PW,
-    database: "test",
-  });
+  host: "localhost",
+  user: "root",
+  password: process.env.DB_PW,
+  database: "test",
+});
 
-app.get("/", (req,res)=>{
-    res.json("hello this is the backend!")
+app.get("/", (req, res) => {
+  res.json("hello this is the backend!")
 })
 
 app.get("/books", (req, res) => {
-    const q = "SELECT * FROM books";
-    db.query(q, (err, data) => {
-      if (err) {
-        console.log(err);
-        return res.json(err);
-      }
-      return res.json(data);
-    });
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
   });
+});
 
-app.listen(8800, ()=>{
-    console.log("Connected to backend!")
+app.post("/books", (req,res)=>{
+  const q = "INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)"
+  const values = [req.body.title,req.body.desc,req.body.cover]
+
+  db.query(q,[values],(err,data)=>{
+    if (err) return res.json(err)
+    return res.json("Book has been created successfully")
+  })
+})
+
+app.listen(8800, () => {
+  console.log("Connected to backend!")
 })
