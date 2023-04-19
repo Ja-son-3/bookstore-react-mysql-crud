3c7e4089e90d76b66452fbd88611a5ab0b02fcc9
Jason Huang <jason97521@gmail.com>
2023-04-19 00:34:46 -0400

    add data to mysql from react

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios'
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Add = () => {
   const [book, setBook] = useState({
@@ -8,13 +10,32 @@ const Add = () => {
     cover: "",
   })
 
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setBook(prev => ({ ...prev, [e.target.name]: e.target.value }))
+  }
+
+  const handleClick = async (e) => {
+    e.preventDefault()
+    try {
+      await axios.post("http://localhost:8800/books", book)
+      navigate("/")
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  console.log(book)
+
   return (
     <div className='form'>
       <h1>Add New Book</h1>
-      <input type="text" placeholder='title' />
-      <input type="text" placeholder='desc' />
-      <input type="number" placeholder='price' />
-      <input type="text" placeholder='cover' />
+      <input type="text" placeholder='title' name="title" onChange={handleChange} />
+      <input type="text" placeholder='desc' name="desc" onChange={handleChange} />
+      <input type="number" placeholder='price' name="price" onChange={handleChange} />
+      <input type="text" placeholder='cover' name="cover" onChange={handleChange} />
+      <button onClick={handleClick}>Add</button>
     </div>
   )
 }
